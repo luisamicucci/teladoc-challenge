7589247225ec50e287a42c8cf31a717e0e9471ec
luisamicucci <luisamicucci2011@gmail.com>
2023-02-24 17:23:57 -0300

    CQRS skelleton added.

---
+++ src/Teladoc.Domain/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Teladoc.Domain.Entities
+{
+    public class User : BaseEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
---
+++ src/TeladocAPI/Models/UserModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TeladocAPI.Models
+{
+    public class UserModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
