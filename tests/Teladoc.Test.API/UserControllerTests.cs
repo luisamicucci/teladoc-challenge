944b77f8cd443c4ed090f8407007fcbb35bdf16a
luisamicucci <luisamicucci2011@gmail.com>
2023-02-25 14:33:47 -0300

    First approach of unit testing.

---
+++ tests/Teladoc.Test.API/UserControllerTests.cs
@@ -0,0 +1,69 @@
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Teladoc.Application.Commands.AddNewUser;
+using Teladoc.Application.Models;
+using Teladoc.Application.Queries.RetrieveUsers;
+using TeladocAPI.Controllers;
+
+namespace Teladoc.Test.API
+{
+    public class UserControllerTests
+    {
+        private readonly UserController _controller;
+        private readonly Mock<IMediator> _mediatorMock;
+        private readonly UserModel _userModelMock = new UserModel
+        {
+            DateOfBirth = DateTime.Now.AddYears(-20),
+            Email = "luis@gmail.com",
+            FirstName = "Luis",
+            LastName = "Amicucci"
+        };
+
+        public UserControllerTests()
+        {
+            _mediatorMock = new Mock<IMediator>();
+            _controller = new UserController(_mediatorMock.Object);
+        }
+
+        [Fact]
+        public async Task Post_ReturnsOkResult()
+        {
+            // Arrange
+            var request = new AddNewUserCommand { User = _userModelMock };
+
+            _mediatorMock.Setup(m => m.Send(request, It.IsAny<CancellationToken>()))
+                         .ReturnsAsync(true);
+
+            // Act
+            var result = await _controller.Post(_userModelMock);
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result);
+            var okResult = result as OkObjectResult;
+            Assert.Equal("User created", okResult.Value);
+        }
+
+        [Fact]
+        public async Task Get_ReturnsOkResult()
+        {
+            // Arrange
+            var request = new RetrieveUsersQuery();
+            var response = new List<UserModel>
+            {
+                _userModelMock
+            };
+
+            _mediatorMock.Setup(m => m.Send(request, It.IsAny<CancellationToken>()))
+                         .ReturnsAsync(response);
+
+            // Act
+            var result = await _controller.Get();
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result);
+            var okResult = result as OkObjectResult;
+            Assert.Equal(response, okResult.Value);
+        }
+    }
+}
